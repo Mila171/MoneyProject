7c41fad87430b648cc9e00777f0a3da17c7a87b6
guest1 <akm17122003@gmail.com>
2021-12-02 12:27:00 +0600

    menu

--- app/src/main/java/com/kamila/money/money.kt
+++ app/src/main/java/com/kamila/money/money.kt
@@ -4,17 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class money : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_money)
+        setContentView(R.layout.activity_main)
+        //todo body
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    //todo переходы
 
-        val exit = findViewById<LinearLayout>(R.id.catalog)
-        exit.setOnClickListener {
-            startActivity(Intent(this,catalog::class.java))
-            finish()
+                    //todo переходы
+                    true
+                }
+                R.id.profile -> {
+                    //todo переходы
+
+                    //todo переходы
+                    true
+                }
+
+                else -> false
+            }
         }
-
+        //todo body
     }
 }
