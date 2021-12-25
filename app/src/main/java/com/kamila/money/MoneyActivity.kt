f19871c2ee829287cdcd4bf90cc87b1e106837de
guest1 <akm17122003@gmail.com>
2021-12-25 15:57:58 +0600

    animation2

--- app/src/main/java/com/kamila/money/MoneyActivity.kt
+++ app/src/main/java/com/kamila/money/MoneyActivity.kt
@@ -12,35 +12,38 @@ class MoneyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        binding= ActivityMoneyBinding.inflate(layoutInflater)
+        binding = ActivityMoneyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.profile.setOnClickListener{
-            startActivity(Intent(this,ProfileActivity::class.java))
+        binding.profile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav)
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav)
 
-        val mOnNavigationItemSelectedListener =
-            BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.purse -> {
-                        startActivity(Intent(this, MoneyActivity::class.java))
-                        return@OnNavigationItemSelectedListener true
+            val mOnNavigationItemSelectedListener =
+                BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+                    when (menuItem.itemId) {
+                        R.id.purse -> {
+                            startActivity(Intent(this, MoneyActivity::class.java))
+                            return@OnNavigationItemSelectedListener true
+                        }
+                        R.id.catalog -> {
+                            startActivity(Intent(this, CatalogActivity::class.java))
+                            return@OnNavigationItemSelectedListener true
+                        }
+                        R.id.history -> {
+                            startActivity(Intent(this, HistoryActivity::class.java))
+                            return@OnNavigationItemSelectedListener true
+                        }
+                        R.id.cards -> {
+                            startActivity(Intent(this, CardsActivity::class.java))
+                            return@OnNavigationItemSelectedListener true
+                        }
                     }
-                    R.id.catalog -> {
-                        startActivity(Intent(this, CatalogActivity::class.java))
-                        return@OnNavigationItemSelectedListener true
-                    }
-                    R.id.history -> {
-                        startActivity(Intent(this, HistoryActivity::class.java))
-                        return@OnNavigationItemSelectedListener true
-                    }
-                    R.id.cards -> {
-                        startActivity(Intent(this, CardsActivity::class.java))
-                        return@OnNavigationItemSelectedListener true
-                    }
+                    false
                 }
-                false
-            }
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                mOnNavigationItemSelectedListener
+            )
+        }
     }
-
+}
